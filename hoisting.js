cea2dca14a80bd469bbcac23ee4a7a823cba1851
Satyasri Varaganti <satyasrib@gmail.com>
2019-01-12 07:24:20 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,4 +1,6 @@
+
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
@@ -10,11 +12,12 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -26,5 +29,7 @@ function sayMyName() {
   function sayMy() {
     console.log(name);
     var name = "Kristin";
+      console.log(name);
   }
+  sayMy();
 }
